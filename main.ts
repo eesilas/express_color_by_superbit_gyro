4dea3ff43c28a327a34676dad4416006598c60c8
Learn STEM by Doing <32452061+eesilas@users.noreply.github.com>
2025-03-05 15:39:34 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,31 @@
-let range = SuperBit.RGB_Program()
-range = SuperBit.RGB_Program().range(0, 3)
+let range = SuperBit.RGB_Program().range(0, 3)
 SuperBit.RGB_Program().showRainbow(1, 360)
 basic.showIcon(IconNames.Asleep)
 basic.forever(function () {
-    SuperBit.RGB_Program().showColor(neopixel.hsl(Math.trunc(input.acceleration(Dimension.X) / 5), Math.trunc(input.acceleration(Dimension.Y) / 5), Math.trunc(input.acceleration(Dimension.X) / 5)))
+    if (input.temperature() < 22) {
+        SuperBit.RGB_Program().showColor(neopixel.colors(NeoPixelColors.Blue))
+    } else if (input.temperature() > 22 && input.temperature() < 23) {
+        SuperBit.RGB_Program().showColor(neopixel.colors(NeoPixelColors.Orange))
+    } else if (input.temperature() > 23 && input.temperature() < 24) {
+        SuperBit.RGB_Program().showColor(neopixel.colors(NeoPixelColors.Yellow))
+    } else if (input.temperature() <= 24 && input.temperature() < 25) {
+        SuperBit.RGB_Program().showColor(neopixel.colors(NeoPixelColors.Green))
+    } else if (input.temperature() <= 25 && input.temperature() < 26) {
+        SuperBit.RGB_Program().showColor(neopixel.colors(NeoPixelColors.Red))
+    } else if (input.temperature() <= 26 && input.temperature() < 27) {
+        SuperBit.RGB_Program().showColor(neopixel.colors(NeoPixelColors.Indigo))
+    } else if (input.temperature() <= 27 && input.temperature() < 28) {
+        SuperBit.RGB_Program().showColor(neopixel.colors(NeoPixelColors.Violet))
+    } else if (input.temperature() <= 28 && input.temperature() < 29) {
+        SuperBit.RGB_Program().showColor(neopixel.colors(NeoPixelColors.Purple))
+    } else if (input.temperature() <= 29 && input.temperature() < 30) {
+        SuperBit.RGB_Program().showColor(neopixel.colors(NeoPixelColors.White))
+    } else {
+        for (let index = 0; index < 4; index++) {
+            SuperBit.RGB_Program().showColor(neopixel.colors(NeoPixelColors.Green))
+            basic.pause(100)
+            SuperBit.RGB_Program().showColor(neopixel.colors(NeoPixelColors.Red))
+            basic.pause(100)
+        }
+    }
 })
